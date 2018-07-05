410f6f58f2fdb7d6f74c6d568e7504b99bcf70c2
Daniela Bulgaru <dbulgaru@pivotal.io>
2018-07-05 11:32:35 +0100

    Implement motion delegate method

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
+let possibleMagic8Answers =
+    ["It is certain.",
+     "It is decidedly so.",
+     "Without a doubt.",
+     "Yes - definitely.",
+     "You may rely on it.",
+     "As I see it, yes.",
+     "Most likely.",
+     "Outlook good.",
+     "Yes.",
+     "Signs point to yes.",
+     "Reply hazy, try again.",
+     "Ask again later.",
+     "Better not tell you now.",
+     "Cannot predict now.",
+     "Concentrate and ask again.",
+     "Don't count on it.",
+     "My reply is no.",
+     "My sources say no.",
+     "Outlook not so good.",
+     "Very doubtful."]
+
 final class ViewController: UIViewController {
 
     @IBOutlet var myLabel: UILabel!
@@ -21,4 +43,18 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        guard let text = myTextField.text, !text.isEmpty else {
+            myLabel.text = "I can't answer what I don't know."
+            return
+        }
+        
+        if motion == .motionShake {
+            let unsignedCount = UInt32(possibleMagic8Answers.count)
+            let number = Int(arc4random_uniform(unsignedCount))
+            
+            myLabel.text = possibleMagic8Answers[number]
+        }
+    }
 }
